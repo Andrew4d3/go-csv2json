8245bb49b48b4daff98401e3c0687d1ad6f5bcaa
Andrew4d3 <andrew4d3@gmail.com>
2020-05-02 21:22:27 -0400

    Adding unit test for writeJSONFile function

--- csv2json_test.go
+++ csv2json_test.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -120,6 +121,55 @@ func Test_processCsvFile(t *testing.T) {
 				if !reflect.DeepEqual(record, wantMap) {
 					t.Errorf("processCsvFile() = %v, want %v", record, wantMap)
 				}
+			}
+		})
+	}
+}
+
+func Test_writeJSONFile(t *testing.T) {
+	dataMap := []map[string]string{
+		{"COL1": "1", "COL2": "2", "COL3": "3"},
+		{"COL1": "4", "COL2": "5", "COL3": "6"},
+	}
+	tests := []struct {
+		csvPath  string
+		jsonPath string
+		pretty   bool
+		name     string
+	}{
+		{"compact.csv", "compact.json", false, "Compact JSON"},
+		{"pretty.csv", "pretty.json", true, "Pretty JSON"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			writerChannel := make(chan map[string]string)
+			done := make(chan bool)
+
+			go func() {
+				for _, record := range dataMap {
+					writerChannel <- record
+				}
+				close(writerChannel)
+			}()
+
+			go writeJSONFile(tt.csvPath, writerChannel, done, tt.pretty)
+
+			<-done
+
+			testOutput, err := ioutil.ReadFile(tt.jsonPath)
+
+			if err != nil {
+				t.Errorf("writeJSONFile(), Output file got error: %v", err)
+			}
+
+			defer os.Remove(tt.jsonPath)
+
+			wantOutput, err := ioutil.ReadFile(filepath.Join("testJsonFiles", tt.jsonPath))
+			check(err)
+
+			if (string(testOutput)) != (string(wantOutput)) {
+				t.Errorf("writeJSONFile() = %v, want %v", string(testOutput), string(wantOutput))
 			}
 		})
 	}
